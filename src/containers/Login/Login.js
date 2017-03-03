cde6af805403e1b7bb509856a0c3cd81337138b9
Daniel Słaby <pufek93@gmail.com>
2017-03-03 08:18:53 +0100

    added basic login screen + react router

---
+++ src/containers/Login/Login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import bemClassName from 'bem-classname'
+import Login from './../../components/Login'
+
+class LoginWrapper extends React.PureComponent {
+
+    constructor() {
+        super()
+        this.classname = bemClassName.bind(null, 'LoginWrapper')
+    }
+
+    render() {
+
+        return (
+            <Login/>
+        )
+    }
+}
+export default LoginWrapper
---
+++ src/containers/Router/Router.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Router, Route, browserHistory} from 'react-router'
+import Login from './../Login'
+
+class RouterWrapper extends React.PureComponent {
+
+    render() {
+
+        return (
+            <Router history={browserHistory}>
+                <Route path="/" component={Login}>
+                </Route>
+            </Router>
+        )
+    }
+}
+export default RouterWrapper
