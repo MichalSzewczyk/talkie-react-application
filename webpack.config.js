0999cfb65cb17590a68229e654864692048c601d
Daniel Słaby <pufek93@gmail.com>
2017-03-03 09:38:37 +0100

    added basic image copying

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,39 @@
 const path = require('path')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const config = {
-  context: path.join(__dirname, 'src/'),
-  devtool: '#inline-source-map',
-  entry: [
-    './index.js'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
-      }
+    context: path.join(__dirname, 'src/'),
+    devtool: '#inline-source-map',
+    entry: [
+        './index.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader'
+            },
+            {test: /(\.css|\.scss)$/, loaders: ['style-loader', 'css-loader', 'sass-loader']},
+            {test: /\.svg/, loader: 'svg-url-loader'},
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: 'index.html'
+    }),
+        new CopyWebpackPlugin([
+            {
+                from:'./resources/image/login_background.jpg',
+                to:'./login_background.jpg'
+            }
+        ])
     ]
-  },
-  plugins: [new HtmlWebpackPlugin({
-    template: 'index.html'
-  })]
 }
 module.exports = config
