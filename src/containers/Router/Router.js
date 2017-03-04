f5d167b038d9fc07b2c92b1ba9280057dd1a3ec5
Daniel Słaby <pufek93@gmail.com>
2017-03-04 17:15:11 +0100

    added react-router-redux

--- src/containers/Router/Router.js
+++ src/containers/Router/Router.js
@@ -2,15 +2,17 @@ import React from 'react'
 import {Router, Route, browserHistory} from 'react-router'
 import Login from './../Login'
 import Register from './../Register'
-
+import store from './../../store'
+import {syncHistoryWithStore} from 'react-router-redux'
+const history = syncHistoryWithStore(browserHistory, store);
 class RouterWrapper extends React.PureComponent {
-  render () {
-    return (
-      <Router history={browserHistory}>
-        <Route path='/' component={Login} />
-        <Route path='/register' component={Register} />
-      </Router>
-    )
-  }
+    render() {
+        return (
+            <Router history={history}>
+                <Route path='/' component={Login}/>
+                <Route path='/register' component={Register}/>
+            </Router>
+        )
+    }
 }
 export default RouterWrapper
