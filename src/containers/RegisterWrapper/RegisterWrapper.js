24daf530aac3dc11440102d815dfb6d8756b17a5
mszewczyk <szewczykm94@gmail.com>
2017-03-09 22:20:51 +0100

    created wrapper for register

--- src/containers/RegisterWrapper/RegisterWrapper.js
+++ src/containers/RegisterWrapper/RegisterWrapper.js
@@ -1,18 +1,32 @@
-import React from 'react'
-import bemClassName from 'bem-classname'
-import Register from './../../components/Register'
+import React from "react";
+import bemClassName from "bem-classname";
+import Register from "./../../components/Register";
+import {connect} from "react-redux";
+import RegisterAction from "./../../actions/register";
 class RegisterWrapper extends React.PureComponent {
 
     constructor() {
-        super()
+        super();
         this.classname = bemClassName.bind(null, 'RegisterWrapper')
     }
 
-    render() {
+	render() {
+		const {registerAction} = this.props;
+		return (
+			<Register register={registerAction}/>
+		)
+	}
 
-        return (
-            <Register/>
-        )
-    }
 }
-export default RegisterWrapper
+
+function mapStateToProps() {
+	return {}
+}
+function mapDispatchToProps(dispatch) {
+	return {
+		registerAction: (mail, password) => {
+			dispatch(RegisterAction(mail, password))
+		}
+	}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(RegisterWrapper)
