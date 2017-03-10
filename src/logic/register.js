a67b1a2dd8a1c2791eb6b12ea30a392af70a6c69
mszewczyk <szewczykm94@gmail.com>
2017-03-10 17:27:24 +0100

    Small change in query

--- src/logic/register.js
+++ src/logic/register.js
@@ -4,7 +4,7 @@ export function requestRegister(login, password) {
 	return new Promise((resolve, reject) => {
 		let query = `
             query{
-              login(login:"${login}", password:"${password}"){
+              register(login:"${login}", password:"${password}"){
                 login
                 password
               }
