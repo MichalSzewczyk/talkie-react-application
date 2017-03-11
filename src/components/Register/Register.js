bfc6fa0d54cb2726960bbe46eca0c2f9f8ec5159
Daniel Słaby <pufek93@gmail.com>
2017-03-11 08:36:53 +0100

    added basic register styling choped register validate function

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,72 +3,80 @@ import bemClassName from "bem-classname";
 import SweetAlert from './../Alert'
 
 class Register extends React.PureComponent {
-  constructor () {
-		super();
-		this.classname = bemClassName.bind(null, 'Register');
-  }
+    constructor() {
+        super();
+        this.classname = bemClassName.bind(null, 'Register');
+    }
 
-	onRegister() {
-		const {register} = this.props;
-		const loginValue = this.refs.login.value;
-		const passwordFirst = this.refs.passwordFirst.value;
-		const passwordSecond = this.refs.passwordSecond.value;
-		if (loginValue === "" || passwordFirst === "" || passwordSecond === "") {
-			let message = "Please provide required values:" + (loginValue === "" ? "\n- login" : "") + (passwordFirst === "" ? "\n- first password" : "") + (passwordSecond === "" ? "\n- second password" : "");
-			swal({title: message, type: "warning", timer: 2000});
-		}
-		else if (passwordFirst !== passwordSecond) {
-			swal({title: "Passwords are different.", type: "warning", timer: 2000});
-		} else {
-			register(loginValue, passwordFirst);
-		}
-	}
-  render () {
-    return (
+    onRegister() {
+        const {register} = this.props;
+        const loginValue = this.refs.login.value;
+        const passwordFirst = this.refs.passwordFirst.value;
+        const passwordSecond = this.refs.passwordSecond.value;
 
-			<div className={this.classname("registerBox")}>
+        if (this.validatePassword(loginValue, passwordFirst, passwordSecond)) {
+            register(loginValue, passwordFirst);
+        }
+    }
+
+    validatePassword(loginValue, passwordFirst, passwordSecond) {
+        if (loginValue === "" || passwordFirst === "" || passwordSecond === "") {
+            const message = "Please provide required values:" + (loginValue === "" ? "\n- login" : "") + (passwordFirst === "" ? "\n- first password" : "") + (passwordSecond === "" ? "\n- second password" : "");
+            swal({title: message, type: "warning", timer: 2000});
+        }
+        else if (passwordFirst !== passwordSecond) {
+            swal({title: "Passwords are different.", type: "warning", timer: 2000});
+        } else {
+            return true;
+        }
+    }
+
+    render() {
+        return (
+
+            <div className={this.classname("registerBox")}>
 			<span tabIndex="4" className={this.classname("registerAccountText")}>
 				Register new account:
 			</span>
-				<div className={this.classname("inputBundle")}>
-					<input
-						className={this.classname("email")}
-						placeholder="Login"
-						tabIndex="1"
-						required="required"
-						ref="login"
-						type='email'/>
-					<input
-						className={this.classname("password")}
-						tabIndex="2"
-						placeholder="Password"
-						required="required"
-						ref="passwordFirst"
-						type='password'/>
-					<input
-						className={this.classname("repeatPassword")}
-						tabIndex="2"
-						placeholder="Repeat password"
-						required="required"
-						ref="passwordSecond"
-						type='password'/>
-				</div>
-				<button
-					className={this.classname("registerButton")}
-					tabIndex="3"
-					onClick={::this.onRegister}>
-					<span>Register</span>
-				</button>
+                <div className={this.classname("inputBundle")}>
+                    <input
+                        className={this.classname("email")}
+                        placeholder="Login"
+                        tabIndex="1"
+                        required="required"
+                        ref="login"
+                        type='email'/>
+                    <input
+                        className={this.classname("password")}
+                        tabIndex="2"
+                        placeholder="Password"
+                        required="required"
+                        ref="passwordFirst"
+                        type='password'/>
+                    <input
+                        className={this.classname("repeatPassword")}
+                        tabIndex="2"
+                        placeholder="Repeat password"
+                        required="required"
+                        ref="passwordSecond"
+                        type='password'/>
+                </div>
+                <button
+                    className={this.classname("registerButton")}
+                    tabIndex="3"
+                    onClick={::this.onRegister}>
+                    <span>Register</span>
+                </button>
 
-				<button
-					className={this.classname("abortButton")}
-					tabIndex="3"
-					onClick={() => location.href = "/"}>
-					<span>Abort</span>
-				</button>
-			</div>
-    )
-  }
+                <button
+                    className={this.classname("abortButton")}
+                    tabIndex="3"
+                    onClick={() => location.href = "/"}>
+                    <span>Abort</span>
+                </button>
+            </div>
+        )
+    }
 }
 
 export default Register
