cc6801ec79a3410590ab004303b55eaee560fc7a
Daniel Słaby <pufek93@gmail.com>
2017-04-17 20:17:53 +0200

    added logout notification on TCP

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -2,9 +2,13 @@ import {push} from 'react-router-redux';
 import {TCP_CONNECTION_DISCONNECT} from './../ducks/chat'
 import {clearLoggedFromLocalStorage} from './../logic/auth'
 import TcpManager from '../tcp/TcpManager'
+import TCP_CONSTANTS from './../constants/TCP'
 
 export default function logoutAction() {
     return (dispatch) => {
+        TcpManager.send({
+            type: TCP_CONSTANTS.sendMessageTypes.LOGOUT
+        })
         TcpManager.close()
         dispatch(TCP_CONNECTION_DISCONNECT())
         clearLoggedFromLocalStorage()
--- src/constants/TCP.js
+++ src/constants/TCP.js
@@ -11,7 +11,8 @@ export default {
         'PING': 'PING',
         'CLOSE_STREAM': 'CLOSE_STREAM',
         'FETCH_USER_STATUS': 'FETCH_USER_STATUS',
-        'FIND_USER': 'FIND_USER'
+        'FIND_USER': 'FIND_USER',
+        'LOGOUT': 'LOGOUT'
     },
     receiveMessageTypes: {
         'RECEIVE_MESSAGE': 'RECEIVE_MESSAGE',
